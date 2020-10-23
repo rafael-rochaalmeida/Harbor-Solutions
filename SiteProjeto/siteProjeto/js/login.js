68547d7a5db84c84c1be77fa102510d037753ce7
MarcusSilva98 <marcus.silva@bandtec.com.br>
2020-10-23 02:25:17 -0300

    validação sem (@)Login

--- SiteProjeto/siteProjeto/js/login.js
+++ SiteProjeto/siteProjeto/js/login.js
@@ -1,13 +1,13 @@
-function logar_sistema(){
+function logar_sistema() {
     var email_user = email.value;
     var senha_user = senha.value;
-    if(email_user == 'harbor@solutions.com.br' && senha_user == 'h123'){
+    if (email_user == 'harborsolutions.com.br' && senha_user == 'h123') {
         login_valido()
-    } else{
+    } else {
         mensagem.innerHTML = "Login inválido!"
     }
 };
-function login_valido(){
+
+function login_valido() {
     window.location.href = "../pages/dashboard.html";
 }
-
