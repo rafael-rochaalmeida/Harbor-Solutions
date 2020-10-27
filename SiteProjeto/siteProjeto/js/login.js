a9fc370cca95438c0013a6adef25fa76c8a24ae9
rafael-rochaalmeida <rafael.almeida@bandtec.com.br>
2020-10-27 01:04:35 -0300

    Alterações na dashboard e validação da tela de login

--- SiteProjeto/siteProjeto/js/login.js
+++ SiteProjeto/siteProjeto/js/login.js
@@ -1,26 +1,15 @@
 function logar_sistema() {
     var email_user = email.value;
     var senha_user = senha.value;
-<<<<<<< HEAD
-    var login_user = "haborsolutions.com.br";
+
+    var login_user = "habor@solutions.com.br";
     if(email_user == login_user && senha_user == 'h123'){
         login_valido();
     } else{
         alert('Senha ou login inválido')
     }
 };
-function login_valido(){
-    window.location.href = "dashboard.html";
-}
-=======
-    if (email_user == 'harborsolutions.com.br' && senha_user == 'h123') {
-        login_valido()
-    } else {
-        mensagem.innerHTML = "Login inválido!"
-    }
-};
->>>>>>> 13ed8ff7ed3a388a00d5f91afaf28f62d369901b
-
+    
 function login_valido() {
-    window.location.href = "../pages/dashboard.html";
+    window.location.href = "../Graficos/Grafico-Line/grafico-line.html";
 }
