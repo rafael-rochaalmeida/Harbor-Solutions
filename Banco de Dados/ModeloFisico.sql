8c481285d24743940fe6d6549f4ec08be004d358
MarcusSilva98 <marcus.silva@bandtec.com.br>
2020-10-27 14:43:31 -0300

    Bando fisico em andamento

--- Banco de Dados/ModeloFisico.sql
+++ Banco de Dados/ModeloFisico.sql
@@ -1,39 +1,38 @@
 create database harbor;
 use harbor;
 
-create table complementoCep(
-idCep char(8) primary key,
+create table Cep(
+idCep int primary key auto_increment,
 lobradouro varchar(45),
-bairro varchar (45)
+bairro varchar (45),
+cep char(8)
 );
 
+insert into Cep values
+(null,'Rua tupiniquins','Jardim Esmeralda','02678989'),
+(null,'Rua Borges Lagoa','Brooklin','09872989'),
+(null,'Rua Emanuel Nobrega','Coronel Gomes','02670009');
 
 create table transportadora (
 idtransportadora int primary key auto_increment,
 nomecliente varchar(45),
 cnpj char (14),
-telefonefixo char (12),
-telefonecelular char (12),
+telefonefixo char (10),
+telefonecelular char (11),
 numero int,
 complemento varchar(45),
-fkcep char(8),foreign key(fkCep) references complementoCep(idCep)
-);
-
-
-
-insert into complementoCep values
-('02678989','Rua tupiniquins','Jardim Esmeralda'),
-('09872989','Rua Borges Lagoa','Brooklin'),
-('02670009','Rua Emanuel Nobrega','Coronel Gomes');
+fkcep int,
+foreign key(fkCep) references Cep(idCep)
+)auto_increment = 100;
 
 
 
 insert into transportadora (nomecliente, cnpj, telefonefixo, telefonecelular, numero, complemento, fkcep) values 
-('Americanas','89874652784765','119390282000','119403246742',230,'Galpão C', '02678989'),
+('Americanas','89874652784765','1193902820','11940324672',230,'Galpão C', '02678989'),
 
-('Golden','89874652789241','119390288798','119403200902',30,'Galpão A', '09872989'),
+('Golden','89874652789241','1193902887','11940320090',30,'Galpão A', '09872989'),
 
-('SeaTravel','89874652780011','119390297500','119408786742',98,'Galpão B', '02670009');
+('SeaTravel','89874652780011','1193902975','11940878674',98,'Galpão B', '02670009');
 
 
 select *from complementoCep,transportadora where idCep = fkcep;
